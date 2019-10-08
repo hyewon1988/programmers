b4028299ef50dbe22221695b9d680f33a1d39c28
monkey <hyewon1988@gmail.com>
2019-10-09 00:28:09 +0900

    add solution2 - bin()

--- 2018_kakao_blind/secret_map.py
+++ 2018_kakao_blind/secret_map.py
@@ -12,3 +12,22 @@ def solution(n, arr1, arr2):
             count += 1
         answer.append(passwords)
     return answer
+
+def solution2(n, arr1, arr2):
+    answer = []
+    for row1, row2 in zip(arr1, arr2):
+        row = bin(row1|row2)[2:]
+        password = row.rjust(n, " ")
+        password = password.replace("1", "#")
+        password = password.replace("0", " ")
+        answer.append(password)
+    return answer
+
+if __name__ == '__main__':
+    n = 6
+    arr1 = [46, 33, 33 ,22, 31, 50]
+    arr2 = [27 ,56, 19, 14, 14, 10]
+
+    print (solution(n, arr1, arr2))
+    print (solution2(n, arr1, arr2))
+    #["######", "### #", "## ##", " #### ", " #####", "### # "
